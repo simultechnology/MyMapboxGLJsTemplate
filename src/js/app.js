52b94851813952432cd99609358eee8b79fa0968
t_ishikawa <t_ishikawa@zenrin-datacom.net>
2019-09-30 18:16:57 +0900

    add flow

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,21 @@
+// @flow
 
 import mapboxgl from 'mapbox-gl'
 
 import '../../dist/css/map.css'
 
 mapboxgl.accessToken = 'your token'
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [139.80, 35.7],
-  zoom: 12
-})
+function createMap (id :string, zoom: number = 12) {
+  const map = new mapboxgl.Map({
+    container: id,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [139.80, 35.7],
+    zoom: zoom
+  })
+  map.addControl(new mapboxgl.ScaleControl({
+    maxWidth: 200,
+    unit: 'metric'
+  }), 'bottom-left')
+}
+
+createMap('map', 12)
